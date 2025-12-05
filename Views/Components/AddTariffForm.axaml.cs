e31bb4405da8fbaafa9467dce2d6c54373e3a646
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-12-05 18:53:40 +0300

    feat: add tarrifs form for consumer

---
+++ Views/Components/AddTariffForm.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ATS_Desktop.Views.Components;
+
+public partial class AddTariffForm : UserControl
+{
+    public AddTariffForm()
+    {
+        InitializeComponent();
+    }
+}

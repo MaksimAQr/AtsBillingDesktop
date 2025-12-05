12d57bdd6f1e32030af27549b0b61f74721ea82b
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-12-05 18:51:50 +0300

    feat: add dialog component for tariffs

---
+++ Views/Components/AddTariffDialog.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ATS_Desktop.Views.Components;
+
+public partial class AddTariffDialog : UserControl
+{
+    public AddTariffDialog()
+    {
+        InitializeComponent();
+    }
+}

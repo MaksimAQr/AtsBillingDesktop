6541d284e8355fb032221aa3ec0e80e81de6f4ae
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-12-05 18:08:24 +0300

    feat: add user list component

---
+++ Views/Components/ListComponent.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia;
+using Avalonia.Controls;
+using System.Collections.ObjectModel;
+using ATS_Desktop.Models;
+
+namespace ATS_Desktop.Views.Components;
+
+public partial class ListComponent : UserControl
+{
+
+
+    public ListComponent()
+    {
+        InitializeComponent();
+    }
+}
